3c7919f373751aea34b703bb6280928430ae2683
shogo.hoshino <shogo.hoshino.10@gmail.com>
2020-12-12 21:34:46 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果:<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
+        <form action="sample.php" method="POST">
+            <buttom type="submit" name="act" vaule="draw">おみくじをひく!</buttom>
+        </form>
+    </body>
+</html>
